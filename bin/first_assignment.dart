549bf413fde0162181238380ef4fe5e7f58a8cbd
Maher Qare <maherqare171@gmail.com>
2025-05-29 19:40:18 +0300

    first assignment answer (calculate function)

--- bin/first_assignment.dart
+++ bin/first_assignment.dart
@@ -1,3 +1,23 @@
+
 void main() {
 
+print(calculate(num1: 10, num2: 15,operation: '*'));
+}
+
+
+num calculate({double num1 = 0,double num2 = 0, String? operation = '+'}){
+
+  switch (operation) {
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2; 
+    case '/':
+      return num1 / num2;
+    case '%':
+      return num1 % num2;
+
+    default:
+      return num1 + num2;
+  }
 }
--- test/first_assignment_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:first_assignment/first_assignment.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
